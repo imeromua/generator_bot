241a1d2254abc0fea1aa92ad518f1eccc2f281ea
imeromua <imerom25@gmail.com>
2026-02-12 12:56:44 +0200

    Register messages and dash routers in common handlers

--- handlers/common.py
+++ handlers/common.py
@@ -5,13 +5,16 @@ This module composes common (user-neutral) handlers and re-exports show_dash.
 
 from aiogram import Router
 
-from handlers.common_parts.dash import show_dash
+from handlers.common_parts.dash import show_dash, router as dash_router
 from handlers.common_parts.help import router as help_router
 from handlers.common_parts.registration import router as registration_router
+from handlers.common_parts.messages import router as messages_router  # FIX #25
 
 
 router = Router()
 router.include_router(registration_router)
 router.include_router(help_router)
+router.include_router(dash_router)  # FIX #25: main_menu callback
+router.include_router(messages_router)  # FIX #25: message history
 
 __all__ = ["router", "show_dash"]
