777a8602ed2abc1f896ea860ebbe0073594dc057
imeromua <imerom25@gmail.com>
2026-03-01 16:56:11 +0200

    Refactor: Empty user.py - all user functions moved to WebApp

--- handlers/user.py
+++ handlers/user.py
@@ -1,19 +1,7 @@
-"""User router aggregator.
-
-This module only composes user-facing routers from handlers.user_parts.
-"""
-
+"""User handlers - empty, all user functions now in WebApp."""
 from aiogram import Router
 
-from handlers.user_parts.events import router as events_router
-from handlers.user_parts.home import router as home_router
-from handlers.user_parts.refill import router as refill_router
-from handlers.user_parts.schedule import router as schedule_router
-from handlers.user_parts.shifts import router as shifts_router
+router = Router(name="user")
 
-router = Router()
-router.include_router(home_router)
-router.include_router(refill_router)
-router.include_router(events_router)
-router.include_router(schedule_router)
-router.include_router(shifts_router)
+# All user functionality moved to Mini App
+# This router is kept for potential future bot-only user commands
